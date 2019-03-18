d2b97c7f8107a5173dfa55501bd1a4c6f8e30b21
allaneff <38565412+allaneff@users.noreply.github.com>
2019-03-18 20:15:01 -0300

    commit final

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+
+        Dados dado = new Dados();
+
+        dado.sorteioNum(4,5,0);
+
+        dado.sorteioSimbolos(true,4,2);
+
+    }
+}
---
+++ src/MessageBox.java
@@ -0,0 +1,6 @@
+//Message box é show
+public class MessageBox {
+    public static void Show(String string){
+        System.out.println(string);
+    }
+}
